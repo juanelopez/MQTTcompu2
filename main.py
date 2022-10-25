9d486ef1da7e9c86a2750720df8a9f88c46128e3
Juan E. Lopez <juanmilo5125@gmail.com>
2022-10-25 19:10:05 -0300

    Servidor web simple para mostrar los logs

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import queue
 from concurrent import futures
 import datetime
 
+
 broker = 'qrio.com.ar'
 port = 1883
 topic = "prod/#"
@@ -35,8 +36,9 @@ def connect_mqtt() -> mqtt_client:
 
 def subscribe(client: mqtt_client,query_mqtt):
     def on_message(client, userdata, msg):        
-        var = msg.payload.decode()
-        query_mqtt.put(var)
+        var = msg.payload.decode()        
+        query_mqtt.put(str(var))
+        query_mqtt.put(str(msg.topic))
         print(f"Received `{var}` from `{msg.topic}` topic")               
     client.subscribe(topic)
     client.on_message = on_message
@@ -68,6 +70,7 @@ async def devolver_peticion(request_recibida,writer):
     archivo = dividir_request[1]
     if(directorio == "/"):
     	directorio = ""
+    '''
     if(archivo != "/"):
         archivo_dividido = []
         archivo_dividido = archivo[1:].split(".")
@@ -77,10 +80,17 @@ async def devolver_peticion(request_recibida,writer):
             extension = archivo_dividido[1]
         else:
             extension = " "
+        print(archivo)
+    '''
     if (archivo == "/"):
         archivo = "/index.html"
         extension = "html"
         dividir_500_extension = ["html"]    
+    else:
+        #print("ESTO")
+        #print(archivo)
+        extension = "txt"        
+        dividir_500_extension = ["txt"]
     version = str.encode(dividir_request[2])    
     if(len(dividir_500_extension) > 1):
         enviar_500 = version + b' 500 Internal Server Error\n'
@@ -91,6 +101,38 @@ async def devolver_peticion(request_recibida,writer):
             writer.write(enviar_500)
         elif(metodo == "GET"):
             try:
+                if(archivo == "/index.html"):
+                    
+                    print("Pantalla de inicio")
+                    request = version+b' 200 OK\n'
+                    content_type = "Content-Type: text/"+extension+"\n"
+                    request_lenght = b'Content-Lenght:20000\n\n'
+                    writer.write(request)
+                    writer.write(bytes(content_type,'utf-8'))
+                    writer.write(request_lenght)
+                    writer.write(bytes("<HTML><HEAD><TITLE>Pantalla de inicio</TITLE></HEAD><BODY>",'utf-8'))
+                    #writer.write(bytes("<P>Hola Compu2</P>",'utf-8'))
+                    for path in os.scandir():
+                        if(path.is_file()):                            
+                            #print(path.name)
+                            #print(path)
+                            link = '<a href="'+path.name+'"''</a>'+path.name+"<br>"                            
+                            writer.write(bytes(link,'utf-8'))
+                    writer.write(bytes("<a href='hola'>hola</a>",'utf-8'))
+                    writer.write(bytes("</BODY></HTML>",'utf-8'))
+                else:
+                    fd1 = os.open(archivo[1:],os.O_RDONLY)
+                    request = version+b' 200 OK\n'
+                    content_type = "Content-Type: text/plain\n"
+                    request_lenght = b'Content-Lenght:20000\n\n'
+                    writer.write(request)
+                    writer.write(bytes(content_type,'utf-8'))
+                    writer.write(request_lenght)             
+                    lectura = os.read(fd1,cantidad_lectura)
+                    while(lectura != b''):
+                        writer.write(lectura)
+                        lectura = os.read(fd1,cantidad_lectura)
+                    os.close(fd1)
                 '''
                 fd1 = os.open(directorio+archivo[1:],os.O_RDONLY)
                 request = version+b' 200 OK\n'
@@ -106,6 +148,7 @@ async def devolver_peticion(request_recibida,writer):
                 os.close(fd1)
                 writer.write("HOLA")                                
                 '''
+                '''
                 fecha = datetime.date.today()
                 fd1 = os.open(str(fecha),os.O_RDONLY)
                 request = version+b' 200 OK\n'
@@ -119,6 +162,8 @@ async def devolver_peticion(request_recibida,writer):
                     writer.write(lectura)
                     lectura = os.read(fd1,cantidad_lectura)
                 os.close(fd1)               
+                '''
+
             except:
                 print("El archivo no existe")				
                 request = version +b' 404 Not Found\n'
@@ -139,32 +184,40 @@ async def webServer():
         await server.serve_forever()
 def runmqtt(query_mqtt):
     print("START MQTT")
-    #client = connect_mqtt()
-    #subscribe(client,query_mqtt)
+    client = connect_mqtt()
+    subscribe(client,query_mqtt)
     #suball(client,query_mqtt)    
-    #client.loop_forever()
-    #asyncio.run(webServer())
+    client.loop_forever()
 def runserver():
     print("START SERVER")
     asyncio.run(webServer())
 def savemqtt(query_mqtt):
     print("GUARDADO")
-    fecha = datetime.date.today()    
+    fecha = datetime.date.today()  
     fdmqtt = os.open(str(fecha),os.O_CREAT|os.O_WRONLY|os.O_APPEND)
     while True:    
         #print (query_mqtt.empty())
         while not query_mqtt.empty():
-            print("LLEGA")
+            #print("LLEGA")
+            
             msg = query_mqtt.get()
-            #msg = msg+'\n'
-            os.write(fdmqtt,bytes(msg,'utf-8'))
+            msg = msg+'\n'
+            hora = datetime.datetime.now().time()
+            hora = str(hora)
+            os.write(fdmqtt,bytes(hora,'utf-8'))
+            os.write(fdmqtt,b' - ')
+            os.write(fdmqtt,bytes(msg,'utf-8'))                 
+            
             #os.close(fdmqtt)
             print(msg)
             if(fecha != datetime.date.today()):
                 os.write(fdmqtt,b'final del dia')
-                os.close(fdmqtt)
+                os.close(fdmqtt)                
                 fecha = datetime.date.today()
                 fdmqtt = os.open(str(fecha),os.O_CREAT|os.O_WRONLY|os.O_APPEND)        
+
+def uploadlogger():
+    print("subiendo a drive")
 
 def pruebaqueue(query_mqtt):    
     print("para enviar")    
@@ -176,16 +229,17 @@ def pruebaqueue(query_mqtt):
         #topico = "prod/test"
 if __name__ == '__main__':
     print("MAIN")
-    query_mqtt = queue.Queue()
+    #print(datetime.datetime.now().time())
     
+    query_mqtt = queue.Queue()    
     hilos = futures.ThreadPoolExecutor()
     hilo_mqtt = hilos.submit(runmqtt,query_mqtt) #hilo de monitor mqtt
     hilo_server = hilos.submit(runserver) #hilo de servidor web 
     hilo_guardado = hilos.submit(savemqtt,query_mqtt) #hilo guardado de mqtt
-    hilo_prueba_queue = hilos.submit(pruebaqueue,query_mqtt)
+    #hilo_prueba_queue = hilos.submit(pruebaqueue,query_mqtt)
     hilo_mqtt.result()
     hilo_server.result()
     hilo_guardado.result()
-    hilo_prueba_queue.result()
+    #hilo_prueba_queue.result()
     
     #run()
