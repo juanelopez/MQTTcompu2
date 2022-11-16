f1670686c222715b5d0ed19e9637b061fe2ff019
Juan Emilio Lopez <juanmilo5125@gmail.com>
2022-11-16 18:19:04 -0300

    Agregando topics mientras esta funcionando el logueo mqtt

--- main.py
+++ main.py
@@ -15,14 +15,16 @@ import filesaver
 import globales as glo
 import multiproceso
 import basedata
+
+defaultTopic =["test/comp2/python","test/comp2/2022"]
+
 def runserver(PORT,cantidad_lectura,directory):
     print("Start server")
     try:
         asyncio.run(webServer.webServer(PORT,cantidad_lectura,directory))
     except:
         print("Server no iniciado")
-def uploadlogger():
-    print("subiendo a drive")
+        exit(1)
 def meterDatos(q):
     for i in range (0,10):
         q.put(i)
@@ -42,31 +44,39 @@ if __name__ == '__main__':
     broker = args.broker
     directory = args.file_dir
         
-    print(type(glo.query_mqtt))
-    #hilos        
+    #print(type(glo.query_mqtt))
+    #Cola de mensajes mqtt multiproceso
     ctx = mp.get_context('spawn')
-    q = ctx.Queue()
-    #hilo_mqtt = glo.hilos.submit(mqtt.runmqtt,glo.query_mqtt,broker,topics) #hilo de monitor mqtt
-    #hilo_server = glo.hilos.submit(runserver,PORT,cantidad_lectura,directory) #hilo de servidor web 
-    #hilo_guardado = glo.hilos.submit(filesaver.savemqtt,glo.query_mqtt,directory) #hilo guardado de mqtt    
+    multiprocq = ctx.Queue()
+    
+    
     print('I AM process id:', os.getpid())
-    meterDatos(q)
-    p = ctx.Process(target=basedata.databaseStart, args=(q,))
+    #proceso de creacion de base de datos
+    p = ctx.Process(target=basedata.databaseStart, args=(multiprocq,))
     p.start()
-    creacionDB = q.get()
+    creacionDB = multiprocq.get()
     if(creacionDB == "No hay base de datos"):
         print("No hay DB, verificar")
     elif(creacionDB == "Configurado"):
         print("Base de datos lista")
     p.join()    
-    p2 = ctx.Process(target=basedata.databasePUT, args=(q,))
-    p2.start()
+    #proceso que recibe por mqtt mensajes y los guarda en la base de datos
+    p2 = ctx.Process(target=basedata.databasePUT, args=(multiprocq,))
+    p2.start()    
+    #meterDatos(multiprocq)
+    #hilos para mqtt, servidor web de visualizacion y guardado en archivos    
+    hilo_mqtt = glo.hilos.submit(mqtt.runmqtt,glo.query_mqtt,broker,topics,multiprocq,glo.query_coms) #hilo de monitor mqtt
+    
+    for sub in defaultTopic:
+        subNew = glo.hilos.submit(mqtt.newSub,sub,glo.query_coms,glo.query_mqtt,multiprocq)
+    #subNew = glo.hilos.submit(mqtt.newSub,"comp2/test/#",glo.query_coms,glo.query_mqtt,multiprocq)    
+    hilo_server = glo.hilos.submit(runserver,PORT,cantidad_lectura,directory) #hilo de servidor web 
+    hilo_guardado = glo.hilos.submit(filesaver.savemqtt,glo.query_mqtt,directory) #hilo guardado de mqtt    
+    hilo_server.result()
+    hilo_guardado.result()                
+    hilo_mqtt.result()
+    #subNew.result()
     p2.join()
-    
-    
-    #hilo_server.result()
-    #hilo_guardado.result()                
-    #hilo_mqtt.result()
 #otro proceso que agregue hilos o procesos(mejor) con ipc en caliente
 #escuchar en ipv4 y v6
 #agregar base de datos con libreria mysql con docker
--- mqtt.py
+++ mqtt.py
@@ -3,8 +3,8 @@ import random
 import globales as glo
 import queue
 port = 1883
-client = None
 client_id = f'python-mqtt-{random.randint(0, 100)}'
+i = 0
 def connect_mqtt(broker) -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
@@ -17,30 +17,35 @@ def connect_mqtt(broker) -> mqtt_client:
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
-def subscribe(client: mqtt_client,query_mqtt,topic):
+def subscribe(client: mqtt_client,query_mqtt,topic,multiprocq):
     def on_message(username,password,msg):        
         query_mqtt.put(str(msg.topic))
-        query_mqtt.put(str(msg.payload.decode()))        
+        query_mqtt.put(str(msg.payload.decode()))  
+        #multiprocq.put(str(glo.i))
+        multiprocq.put(str(msg.topic)+"DELIMITA"+str(msg.payload.decode()))    
+          
         print(f"Mensaje `{msg.payload.decode()}` de `{msg.topic}` topic")               
     client.subscribe(topic)
     print("TOPIC SUB = ",topic)
     client.on_message = on_message
-def runmqtt(query_mqtt,broker,topic):
+def runmqtt(query_mqtt,broker,topic,multiprocq,query_coms):
     print("START MQTT")
     client = connect_mqtt(broker)  
     print("Cliente es",client)  
+    query_coms.put(client)
     for sub in topic:        
-        glo.hilos.submit(subscribe,client,query_mqtt,sub) #creando un hilo por topic
+        glo.hilos.submit(subscribe,client,query_mqtt,sub,multiprocq) #creando un hilo por topic
     client.loop_forever()
-def newSub(topic):
+def newSub(topic,query_coms,query_mqtt,multiprocq):
     print("Agregando topic: ",topic)
-    topic = "raspberry/#"
+    #topic = "raspberry/#"        
     while True:
-        if(client != 0):
-            print("Nuevo topic iniciado")
-            #glo.hilos.submit(subscribe,client,glo.query_mqtt,topic)
-            #client.on_message = on_message            
-            break
-        else:
-            #print("Todavia no inicia el broker")
-            pass
+        try:
+            client = query_coms.get()
+            print("Si",client)
+            glo.hilos.submit(subscribe,client,query_mqtt,topic,multiprocq)
+            query_coms.put(client)
+            return;        
+        except queue.Empty:
+            # handle exception
+            print("Reintentando...")
